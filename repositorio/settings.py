c24338963fa5ffb61c3a88e3f1804f22a71e32f6
mobj990516-arch <mobj990516@gmail.com>
2025-12-09 11:30:46 -0600

    Change database settings to use dj_database_url
    Updated database configuration to use dj_database_url for parsing DATABASE_URL.

--- repositorio/settings.py
+++ repositorio/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from decouple import config
 from pathlib import Path
+import dj_database_url
 import os
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,11 +112,13 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
